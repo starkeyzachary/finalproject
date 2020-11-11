6ba493af5ba87fc2736d70568fa19019d24f02f9
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-10 21:09:49 -0700

    templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'crispy_forms',
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
