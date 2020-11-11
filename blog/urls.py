a5f447177cbee1984fd262a43f3189c33440eee7
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-11 09:34:48 -0700

    templates

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
+    path('about/', views.about, name='about'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     # path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
