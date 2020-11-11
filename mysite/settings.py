ffb56e34f8290a09a6dc8f1ba878e3497bfe2e3f
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-10 21:06:38 -0700

    templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -152,3 +152,6 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+# Django crispy forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
