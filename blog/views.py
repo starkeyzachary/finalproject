552c5772e042e949b1f9fba42ac76f361a60bb07
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-11 18:56:16 -0700

    templates

--- blog/views.py
+++ blog/views.py
@@ -113,21 +113,15 @@ def post_search(request):
     query = None
     results = []
     if 'query' in request.GET:
-        search_form = SearchForm(request.GET)
-        if search_form.is_valid():
-            query = search_form.cleaned_data['query']
-
-            search_vector = SearchVector('title', weight='A') + \
-                            SearchVector('body', weight='B')
-            search_query = SearchQuery(query)
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query),
-            ).filter(rank__gte=0.3).order_by('-rank')
-
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
     return render(request,
                   'blog/post/search.html',
-                  {'search_form': search_form,
+                  {'form': form,
                    'query': query,
                    'results': results})
 
