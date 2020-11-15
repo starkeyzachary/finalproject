ee16aa42b052d0b78d1568d9bdebbace0f15a226
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-15 00:03:10 -0700

    templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,8 +19,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
