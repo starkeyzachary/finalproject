d6449715a20fb468e75786a370e33ac86e6eb6d9
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-14 19:59:29 -0700

    templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['eattraveldogs.com', 'www.eattraveldogs.com', 'localhost', '127.0.0.1']
 ALLOWED_HOSTS = ['*']
@@ -161,7 +161,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATICFILES_DIRS = ()
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA/UPLOADS
 MEDIA_URL = '/media/'
