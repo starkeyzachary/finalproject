f67da40f4cf7cdab46a73f873a4a4e2aa51b7c2e
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-16 21:06:28 -0700

    templates

--- account/views.py
+++ account/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from .models import Profile
+# from django.contrib import messages
+# from .models import Profile
 from .forms import LoginForm, UserRegistrationForm, \
                    UserEditForm, ProfileEditForm
 
@@ -30,11 +30,11 @@ def user_login(request):
     return render(request, 'account/login.html', {'form': form})
 
 
-@login_required
-def dashboard(request):
-    return render(request,
-                  'account/dashboard.html',
-                  {'section': 'dashboard'})
+# @login_required
+# def dashboard(request):
+#     return render(request,
+#                   'account/dashboard.html',
+#                   {'section': 'dashboard'})
 
 
 # def register(request):
