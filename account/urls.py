5dd937abec1bce66b3614eda2a6eac8a0593199f
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-16 21:12:15 -0700

    templates

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
-# from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
 #     previous login view
-    path('login/', views.user_login, name='login'),
+    # path('login/', views.user_login, name='login'),
 
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     # change password urls
     # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
@@ -19,7 +19,7 @@ urlpatterns = [
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
-    # path('', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     # path('', include('django.contrib.auth.urls')),
     # path('register/', views.register, name='register'),
     # path('edit/', views.edit, name='edit'),
