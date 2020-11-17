a783d2b0afb3a73133d8c806ce97479502cafa72
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-16 20:55:42 -0700

    templates

--- account/urls.py
+++ account/urls.py
@@ -3,11 +3,11 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    # previous login view
-    # path('login/', views.user_login, name='login'),
+    previous login view
+    path('login/', views.user_login, name='login'),
 
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     # change password urls
     # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
