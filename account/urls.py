a267526f4304efd7f7d2c0f5d6eb0ad05ef9a8f8
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-17 01:29:51 -0700

    templates

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 # post views
     # path('login/', views.user_login, name='login'),
-    # path('', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
