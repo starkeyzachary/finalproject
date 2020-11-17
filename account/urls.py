da895d8d2d2a24e12af2d5957fc999ab46bbce42
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-16 20:56:55 -0700

    templates

--- account/urls.py
+++ account/urls.py
@@ -3,8 +3,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    previous login view
-    path('login/', views.user_login, name='login'),
+#     previous login view
+#     path('login/', views.user_login, name='login'),
 
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
@@ -19,8 +19,8 @@ urlpatterns = [
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
-    # path('', views.dashboard, name='dashboard'),
-    # path('', include('django.contrib.auth.urls')),
-    # path('register/', views.register, name='register'),
-    # path('edit/', views.edit, name='edit'),
+    path('', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
