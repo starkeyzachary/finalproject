4f24eb27c1e4cb4f2c88532337064517f5929977
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-16 21:30:35 -0700

    templates

--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,6 @@ from django.contrib.postgres.search import TrigramSimilarity
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
-# from django.http import HttpResponse
-# from django.contrib.auth import authenticate, login
-# from .forms import LoginForm
 
 
 def post_list(request, tag_slug=None):
@@ -142,25 +139,3 @@ def about(request):
 
 def contact(request):
     return render(request, 'blog/contact.html')
-
-
-# def user_login(request):
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request,
-#                                 username=cd['username'],
-#                                 password=cd['password'])
-#             if user is not None:
-#                 if user.is_active:
-#                     login(request, user)
-#                     return HttpResponse('Authenticated '\
-#                                         'successfully')
-#                 else:
-#                     return HttpResponse('Disabled account')
-#             else:
-#                 return HttpResponse('Invalid login')
-#     else:
-#         form = LoginForm()
-#     return render(request, 'account/login.html', {'form': form})
