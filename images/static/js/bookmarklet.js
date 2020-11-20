293dca48b42da96c1a1b9435d45a11317c25dbdc
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-19 17:17:15 -0700

    templates

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -1,5 +1,5 @@
 (function(){
-  var jquery_version = '3.4.1';
+  var jquery_version = '3.5.1';
   var site_url = 'https://www.eattraveldogs.com';
   var static_url = site_url + 'static/';
   var min_width = 100;
